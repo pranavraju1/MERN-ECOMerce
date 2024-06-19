2b6daafb944ac83071d4232400bf64c6a0093bb9
Pranav Raju <pranavraju2020@gmail.com>
2024-06-19 20:58:34 +0530

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
